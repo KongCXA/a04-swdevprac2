cd4c5b2450e9cbc85b143a59a98d7aaadd3c3c3f
KongCXA <yecgaa2344@gmail.com>
2025-08-27 21:05:53 +0700

    bug fix

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -2,7 +2,6 @@ export default function BookingPage() {
   return (
     <div className="space-y-3">
       <h1 className="text-2xl font-bold">Venue Booking</h1>
-      <p className="text-gray-600">Coming soon.</p>
     </div>
   );
 }
